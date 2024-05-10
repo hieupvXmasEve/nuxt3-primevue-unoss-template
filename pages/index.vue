bc7e825234fbd33bcfceeb070d40dabdee544c63
hieupv <hieupv@admin.test.com>
2024-05-10 14:34:48 +0700

    setup project

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="mx-auto container">
+    <Suspense>
+      <ClientOnly>
+        <div class="text-center text-purple">
+          Hello World!
+        </div>
+      </ClientOnly>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
---
+++ uno.config.ts
@@ -0,0 +1,37 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    ['flex-center', 'flex items-center justify-center'],
+    ['grid-center', 'grid place-items-center'],
+    ['main-card', 'p-10 sm:mx-0 mx-3'],
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+    }),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+})
